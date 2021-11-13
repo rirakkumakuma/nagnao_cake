da79cae59786911f4d70d90458591a04cdbdfd9f
EC2 Default User <ec2-user@ip-172-31-19-179.ec2.internal>
2021-11-13 14:52:41 +0000

    [Finish]データベース作成

---
+++ db/migrate/20211113134715_remove_created_at_from_customers.rb
@@ -0,0 +1,5 @@
+class RemoveCreatedAtFromCustomers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :customers, :created_at, :datetime
+  end
+end
---
+++ db/migrate/20211113134803_remove_updated_at_from_customers.rb
@@ -0,0 +1,5 @@
+class RemoveUpdatedAtFromCustomers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :customers, :updated_at, :datetime
+  end
+end
