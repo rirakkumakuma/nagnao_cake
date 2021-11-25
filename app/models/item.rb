23706c7973409d9089fccffbaff61a102cdce234
EC2 Default User <ec2-user@ip-172-31-19-179.ec2.internal>
2021-11-25 14:52:00 +0000

    {Finish}商品追加機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :cart_items,dependent: :destroy
+  has_many :order_details,dependent: :destroy
+  attachment :image
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 end
--- app/models/oder.rb
+++ app/models/oder.rb
@@ -1,2 +1,4 @@
 class Oder < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details,dependent: :destroy
 end
